d173e105da71deb7977844b1c40b6d26564bdc5b
lago-pete <113543860+lago-pete@users.noreply.github.com>
2026-07-13 08:39:57 -0700

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int maxSize = 0; 
+        
+        int left = 0;
+        int right = heights.size() - 1;
+
+        while(left < right){
+            if(((right-left) * min(heights[left], heights[right])) > maxSize){
+                maxSize = (right - left) * min(heights[left], heights[right]);
+            }
+            
+            
+            if(heights[left] > heights[right])
+            {
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+        return maxSize;
+
+
+
+    }
+};
