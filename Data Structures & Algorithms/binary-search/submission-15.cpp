b47088b65b6801b814dcf3e7fe69c0be91b2fea4
lago-pete <113543860+lago-pete@users.noreply.github.com>
2026-07-20 09:17:38 -0700

    Add: binary-search - submission-15

---
+++ Data Structures & Algorithms/binary-search/submission-15.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        
+
+        while(left <= right){
+            int mid = left + ((right - left)/2);
+
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[left] == target){
+                return left;
+            }
+            else if(nums[right] == target){
+                return right;
+            }
+            
+            if(nums[mid] > target){
+                right = mid-1;
+            }
+            else if(nums[mid] < target){
+                left = mid + 1;
+            }
+            
+            
+        }
+        return -1;
+
+
+    }
+};
