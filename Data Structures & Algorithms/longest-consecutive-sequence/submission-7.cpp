75b87f391827a2ce929ad85be3b1ea0ffb321188
lago-pete <113543860+lago-pete@users.noreply.github.com>
2026-09-16 11:51:57 -0700

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.cpp
@@ -0,0 +1,40 @@
+class Solution {
+   public:
+    int longestConsecutive(vector<int>& nums) {
+        int ans;
+        int max = 0;
+        int current = 0;
+        int prev_val;
+        sort(nums.begin(), nums.end());
+        for (int num : nums) {
+            cout << num;
+        }
+
+        if(nums.size() == 1){
+            return 1;
+        }
+    
+        for (int i = 0; i < nums.size(); i++) {
+
+            if(prev_val == nums[i]){
+                continue;
+            }
+            if (prev_val == nums[i] - 1) {
+                current++;
+                prev_val = nums[i];
+                if (current > max) {
+                    max = current;
+                }
+            } else {
+                current = 1;
+                prev_val = nums[i];
+                if (current > max) {
+                    max = current;
+                }
+            }
+        }
+        
+        ans = max;
+        return ans;
+    }
+};
